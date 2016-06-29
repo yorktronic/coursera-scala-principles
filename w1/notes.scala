4af2d2a2af4b99f5ac7b4a07c3bd250d7ba28fce
yorktronic <ty.walker@gmail.com>
2016-06-29 16:34:06 -0700

    first commit

---
+++ w1/notes.scala
@@ -0,0 +1,1 @@
+/* This file contains coding notes for week 1 of Coursera's Functional Programming in Scala - Functional Programming Principles */
---
+++ w1/test-project/src/main/scala/test.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Ty on 6/29/2016.
+  */
+package test
+
+object test extends App {
+  println("Hello, world!")
+}
